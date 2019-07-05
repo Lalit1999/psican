7cbb123b84a47b7bf5d57ec46d7d3f62cb6e46c0
manan999 <aggarwalmanan4@gmial.com>
2019-07-05 15:10:48 +0530

    started App

--- src/App.js
+++ src/App.js
@@ -1,4 +1,13 @@
 import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom' ;
+
+// import Home from './comps/Home/Home.js' ;
+// import About from './comps/about/About.js' ;
+// import Contact from './comps/contact/Contact.js' ;
+// import NotFound from './comps/Home/NotFound.js' ;
+// import Header from './comps/Header/Header.js' ;
+// import TopBar from './comps/Header/TopBar.js' ;
+// import './app.css' ;
 
 class App extends React.Component
 {
@@ -6,19 +15,31 @@ class App extends React.Component
 	render()
 	{
 		return(
-			<div>
-				This is App.	      		
-			</div>
+			<div className="App">
+		        <BrowserRouter>
+		          <div>
+		          	<TopBar />
+		            <Header />
+		            <Switch>
+		              <Route path='/' exact component={Home} />
+		              <Route exact component={NotFound} />
+		            </Switch>
+		          </div>
+		        </BrowserRouter>
+		      </div>
 		) ;
 	}
 }
 
 export default App;
-// home 
-//not found
-//header 
-// top bar
-
-
-//about
-//contact
+		              // <Route path='/about/leader' exact component={}/>
+		              // <Route path='/about/vision' exact component={}/>
+		              // <Route path='/about/psican' exact component={}/>
+		              // <Route path='/contact' exact component={}/>
+		              // <Route path='/mentamorph' component={}/>
+		              // <Route path='/login' component={} />
+		              // <Route path='/register' component={} />
+		              // <Route path='/program/KFMP' component={}/>
+		              // <Route path='/program/AEQS' component={}/>
+		              // <Route path='/program/test' component={}/>
+		              // <Route path='/program/personal' component={}/>
