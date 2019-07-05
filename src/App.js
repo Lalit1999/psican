6db4d9b67c774977a85a0df603e7f8112cabf29c
Lalit1999 <9lalitkumar9@gmail.com>
2019-07-05 11:28:29 +0530

    added libraries

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+class App extends React.Component
+{
+	
+	render()
+	{
+		return(
+			<div>
+				This is App.	      		
+			</div>
+		) ;
+	}
+}
+
+export default App;
+// home 
+//not found
+//header 
+// top bar
+
+
+//about
+//contact
