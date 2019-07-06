99790a03e834523afdb1e630b13d73cd3fd9d621
Lalit1999 <9lalitkumar9@gmail.com>
2019-07-06 22:58:38 +0530

    added map in footer

--- src/comps/footer/Footer.js
+++ src/comps/footer/Footer.js
@@ -6,8 +6,17 @@ class Footer extends React.Component
 	render()
 	{
 		return(
-			<div className = 'center'>
-				This is Footer.
+			<div>
+				<div className = 'map'>
+					<div className="mapouter">
+						<div className="gmap_canvas">
+							<iframe title = "map" width="1100" height="400" id="gmap_canvas" 
+							src="https://maps.google.com/maps?q=magolpuri%20market%20&t=&z=13&ie=UTF8&iwloc=&output=embed" 
+							frameBorder="0" scrolling="no" marginHeight="0" marginWidth="0"></iframe>
+						</div>
+					</div>
+				</div>
+				
 			</div>
 		) ;
 	}
