68c863433fe5799b5bf5476bd343a59801044ce9
Lalit1999 <9lalitkumar9@gmail.com>
2019-07-07 21:05:37 +0530

    footer completed

--- src/comps/footer/Footer.js
+++ src/comps/footer/Footer.js
@@ -35,18 +35,20 @@ class Footer extends React.Component
 					<div className = 'map'>
 						<div className="mapouter">
 							<div className="gmap_canvas">
-								<iframe title = "map" width="1100" height="400" id="gmap_canvas" 
+								<iframe title = "map" width="600" height="400" id="gmap_canvas" 
 								src="https://maps.google.com/maps?q=magolpuri%20market%20&t=&z=13&ie=UTF8&iwloc=&output=embed" 
 								frameBorder="0" scrolling="no" marginHeight="0" marginWidth="0"></iframe>
 							</div>
 						</div>
 					</div>
 					<div>
-						<h3>Contact Information</h3>
-						<div>
-							<p><FontAwesomeIcon icon = {faMap} />&nbsp;{this.state.data.address}</p>
-							<p><FontAwesomeIcon icon = {faPhoneAlt} />&nbsp;{this.state.data.phone}</p>
-							<p><FontAwesomeIcon icon = {faEnvelope} />&nbsp;{this.state.data.email}</p>
+						<div className = 'conte'>
+							<h3><ins>Contact Information</ins></h3>
+							<div>
+								<p><FontAwesomeIcon icon = {faMap} />&nbsp;{this.state.data.address}</p>
+								<p><FontAwesomeIcon icon = {faPhoneAlt} />&nbsp;{this.state.data.phone}</p>
+								<p><FontAwesomeIcon icon = {faEnvelope} />&nbsp;{this.state.data.email}</p>
+							</div>
 						</div>
 					</div>
 				</div>
