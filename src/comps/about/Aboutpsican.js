d7a0f3fb72fc1b98d1b02aac54934ca1f88c971d
Lalit1999 <9lalitkumar9@gmail.com>
2019-07-08 11:34:28 +0530

    changes in display

--- src/comps/about/Aboutpsican.js
+++ src/comps/about/Aboutpsican.js
@@ -15,7 +15,7 @@ class Aboutpsican extends React.Component
 					<Title name = 'P S I C A N' items={["Home -"," Programs -", "PSICAN"]}/>
 				</div>
 				<div>
-					<Display name= "Psican" clr="red" img={Image2} content = "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+					<Display name= "Psican" clr="b" img={Image2} content = "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
 							tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
 							quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
 							consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
--- src/comps/about/Aboutvision.js
+++ src/comps/about/Aboutvision.js
@@ -14,7 +14,7 @@ class Aboutpsican extends React.Component
 					<Title name = 'Our Vision' items={["Home -"," Programs -", "Vision"]}/>
 				</div>
 				<div>
-					<Display name= "Our Vision" clr="red" img={Image3} content = "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+					<Display name= "Our Vision" clr="g" img={Image3} content = "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
 							tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
 							quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
 							consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
