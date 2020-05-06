c917f067736112a3e546963503805d390b959ecb
manan999 <aggarwalmanan4@gmial.com>
2020-05-06 14:00:07 +0530

    date created

--- src/comps/program/Personal.js
+++ src/comps/program/Personal.js
@@ -1,8 +1,13 @@
 import React from 'react' ;
+import DatePicker from 'react-datepicker' ;
+import "react-datepicker/dist/react-datepicker.css";
 
 import Title from '../title/Title.js' ;
 import DisplayDetailed from '../display/DisplayDetailed.js' ;
 import Heading from '../Heading/Heading.js' ;
+import LoginForm from '../signup/forms/LoginForm.js' ;
+import TextArea from '../signup/text/TextArea.js' ;
+import Text from '../signup/text/Text.js' ;
 import './program.css' ;
 
 const features = [
@@ -21,9 +26,88 @@ const ptData = [ 'Bereavement Adjustment', 'Accident / Disease',
 const mData = [ 'Personality Re-Modelling', 'Relational Re-Modelling' ];
 
 class Personal extends React.Component
-{
+{	state = {
+		error: '',
+		reason : '',
+		title: '',
+		minTime: 17,
+		maxTime: 19,
+		date: new Date()
+	}
+
+	returnTomorrow = () => {
+		const tom = new Date() ;
+		tom.setDate(tom.getDate() + 2) ;
+		tom.setHours(18) ;
+		tom.setMinutes(0) ;
+		return tom ;
+	}
+
+	returnTime = (hour) => {
+		const tim = new Date() ;
+		tim.setHours(hour) ;
+		return tim ;
+	}
+
+	componentDidMount = () => {
+		this.setState({date: this.returnTomorrow()});
+	}
+
+	onScheduleClick = () => {
+	  	if(this.state.error !== '')
+			this.setState({error: 'You cannot proceed without fixing all the errors'});
+	  	else if(this.state.title === '')
+			this.setState({error: 'Title can not be blank'});
+		else if(this.state.reason === '')
+			this.setState({error: 'Reason can not be blank'});
+		else if(this.state.date.getDay() === 6)
+		{	if(this.state.date.getHours() < 9 || this.state.date.getHours() > 13)
+				this.setState({error: 'Invalid Date or Time range'});
+			else 
+				console.log(this.state) ;
+		}
+		else if(this.state.date.getDay() === 0)
+			this.setState({error: 'Invalid Date or Time range'});
+		else if(this.state.date.getHours() < 18 || this.state.date.getHours() > 20)
+			this.setState({error: 'Invalid Date or Time range'});
+		else
+			console.log(this.state) ;
+	}
+
+	filterDates = (date) => {
+    	const day = date.getDay();
+    	return day !== 0 ;
+	}
+
+	onTitleChange = (event) => {
+		if(event.target.value === '')
+			this.setState({error: 'Title can not be blank'}) ;
+		else
+		{	if(this.state.error === 'Title can not be blank') 
+				this.setState({error: ''}) ;
+		}
+		this.setState({title : event.target.value}) ;
+	}
+	
+	onReasonChange = (event) => {
+		if(event.target.value === '')
+			this.setState({error: 'Reason can not be blank'}) ;
+		else
+		{	if(this.state.error === 'Reason can not be blank') 
+				this.setState({error: ''}) ;
+		}
+		this.setState({reason : event.target.value} ) ;
+	}
+
+	onDateChange = (date) => {
+		if(date.getDay() === 6)
+			this.setState({ date: date, minTime: 8, maxTime:13, error: ''})
+		else
+			this.setState({ date: date, minTime: 17, maxTime:19, error: ''})
+	}
+
 	render()
-	{
+	{	const {reason, title, date, minTime, maxTime} = this.state ;
 		return(
 			<div>
 				<Title name = 'Personal Consultation' items={["Home -"," Programs -", "Consult"]}/>
@@ -48,6 +132,22 @@ class Personal extends React.Component
 				<DisplayDetailed title="Personality Appraisal Counselling" small="yes" lidata={pData} />
 				<DisplayDetailed title="Post Trauma Counselling" small="yes" lidata={ptData} />
 				<DisplayDetailed title="Metamorphosis Counselling" small="yes" lidata={mData} />
+				<Heading text="Schedule Your Appointment" />
+				<div className="blue-bg">
+					<LoginForm title=" Schedule " error={this.state.error} >
+						<Text label="Title" value={title} onChange={this.onTitleChange} />
+						<TextArea label="Consultation&nbsp;&nbsp; Reason" value={reason} r={4} c={20} onChange={this.onReasonChange} />
+						<div className="date-cont">
+							<label className="lbel">Select Date&nbsp; : </label>
+							<DatePicker selected={date} onChange={this.onDateChange}
+						      showTimeSelect timeFormat="HH:mm" timeIntervals={30} 
+						      filterDate={this.filterDates} minDate={this.returnTomorrow()}
+						      minTime={this.returnTime(minTime)} maxTime={this.returnTime(maxTime)}
+						      timeCaption="Time" dateFormat="MMMM d, yyyy h:mm aa" />
+					    </div>
+					</LoginForm>	<br/>
+					<button onClick={this.onScheduleClick} className="sched-btn"> Check Availablity ! </button> 
+				</div>
 			</div>
 		) ;
 	}
--- src/comps/slider/TestimonialSlider.js
+++ src/comps/slider/TestimonialSlider.js
@@ -4,7 +4,7 @@ import Swiper from 'react-id-swiper';
 import i1 from '../images/i1.jpg' ;
 import i2 from '../images/i2.jpg' ;
 import i3 from '../images/i3.jpg' ;
-import i4 from '../images/i4.jpg' ;
+import i4 from '../images/i5.jpg' ;
 
 import './testimonial.css' ;
 
