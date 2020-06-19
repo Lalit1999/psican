5523453e7bea0f7480a87a18c878306ff1aebbd1
manan999 <aggarwalmanan4@gmial.com>
2020-06-19 11:39:15 +0530

    about person re-created

--- src/comps/about/Aboutperson.js
+++ src/comps/about/Aboutperson.js
@@ -17,31 +17,31 @@ class Aboutperson extends React.Component
 				<div className = 'flex'>
 					<div className="about-left">
 						<div className = 'font about-bold'>Ashish Aggarwal</div>
-						<ul> <div className = 'about-bold'>Academic Journey</div> 
-							<li>Upto X : Mahavir Senior Model School, Delhi</li>
-							<li>XI & XII : GBSSS No. 1 Shakti Nagar, Delhi</li>
-							<li>D. Pharmacy : DIPSAR, Delhi</li>
-							<li>B.Sc (Applied Psychology) : Annamalai University</li>
-							<li>M.Sc (Applied Psychology) : Annamalai University</li>
-							<li>PGD.GC : Annamalai University</li>
+						<ul className="about-list"> 
+							<li>Counsellor, Mentor, Motivator</li>
+							<li>B.Sc & M.Sc (Applied Psychology) </li>
+							<li>Post-Graduate Diploma in Guidance & Counselling</li>
+							<li>Diploma in Pharmacy</li>
+							<li>Diploma in System Management</li>
 						</ul>
-						<ul> <div className = 'about-bold'>Professional Journey </div>
-							<li>1993-1998 : Job in Sun Pharma Ltd., NATCO Pharma Ltd. and USV Ltd.</li>
-							<li>1998-2012 : Propreitor Kasturi Enterprises (Pharma Distribution)</li>
-							<li>2006-Till Date : Managing Director, ORN Remedies Private Ltd.
-								(Ayurvedic & Life-Style Products)</li>
-						</ul>
-						<ul> <div className = 'about-bold'>Social Profile</div>
-							<li>2003-Till Date : Core Committee Member at Udayan shallini 
-								Fellowship Program of NGO "Udayan Care".</li>
-							<li>2002-Till Date : Associated with MSMS Delhi in various
-							 advisory roles and presently the Vice-Chairman of PTA.</li>
-						</ul>
-						<div className = 'txt'>DOB : 13 September 1973.</div>
 					</div>
 					<div className = 'photo br'>
 						<img className ='br' src = {Leader_image} alt = "Leader"/>
 					</div>
+				</div>
+				<div className="after-div">
+					<ul> 
+						<li className = 'txt'>Director&emsp;:&emsp;Psyment</li>
+						<li className = 'txt'>Managing Director&emsp;:&emsp;ORN Remedies Pvt. Ltd. (A 
+							Pharmaceutical Company based in Delhi) </li>
+						<li className = 'txt'>Hails an illustrous multi-year association with esteemed
+							organisations like Mahavir Senior Model School (Delhi), NGO - Udayan Care
+							(Delhi), Tatva Pranic Healing (Pune) as a trainer and motivator</li>
+						<li className = 'txt'>He has been born and brought-up in Delhi. Most of his 
+							education has also been based in Delhi.</li>
+						<li className = 'txt'>In the initial part of his career, he served in top 
+							Pharmaceutical companies like Sun Pharma, Natco Pharma & USV Ltd. </li> 
+					</ul>
 				</div>
 			</div>
 		) ;
