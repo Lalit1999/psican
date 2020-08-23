f61a23c57fb49a09be5b940e01a14c5161a92305
manan999 <aggarwalmanan4@gmial.com>
2020-08-23 12:37:53 +0530

    pwa try

--- src/index.js
+++ src/index.js
@@ -2,10 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App.js';
+import * as serviceWorker from './serviceWorker';
 
-const rootElement = document.getElementById('root');
-if (rootElement.hasChildNodes()) {
-    ReactDOM.hydrate(<App />, rootElement);
-} else {
-    ReactDOM.render(<App />, rootElement);
-}
+// const rootElement = document.getElementById('root');
+// if (rootElement.hasChildNodes()) {
+//     ReactDOM.hydrate(<App />, rootElement);
+// } else {
+//     ReactDOM.render(<App />, rootElement);
+// }
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+serviceWorker.register() ;
