5998253fe9fe6a4896e036afa93812c73be41347
manan999 <aggarwalmanan4@gmail.com>
2020-09-02 10:32:41 +0530

    Added second embed

--- src/comps/footer/Footer.js
+++ src/comps/footer/Footer.js
@@ -1,8 +1,25 @@
 import React from 'react' ;
-import { faPhoneAlt, faEnvelope, faMap} from '@fortawesome/free-solid-svg-icons' ;
+import { faPhoneAlt, faEnvelope, faMap/*, faMapMarkerAlt*/} from '@fortawesome/free-solid-svg-icons' ;
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome' ;
+// import GoogleMapReact from 'google-map-react' ;
 
 import './Footer.css' ;
+
+// const location = {
+//   address: 'CSC, Unit No. 4, First Floor, Sector A, Pocket B&C, Phase 4, Ashok Vihar, Delhi, 110052',
+//   lat: 28.6838276,
+//   lng: 77.1669911,
+// }
+
+// const LocationPin = ({ text }) => (
+  // <div className="pin">
+    // <FontAwesomeIcon icon = {faMapMarkerAlt} />
+    // <p className="pin-text">{text}</p>
+//   </div>
+// )
+						// <GoogleMapReact bootstrapURLKeys={{ key: 'AIzaSyDyiIVEfjI_mTICG90FWJ-Xnj-3c_1wCRc' }} defaultCenter={location} defaultZoom={17} >
+					 //       <LocationPin lat={location.lat} lng={location.lng} text={location.add} />
+					 //    </GoogleMapReact>
 
 class Footer extends React.Component
 {	
@@ -13,8 +30,7 @@ class Footer extends React.Component
 					<div className = 'map'>
 						<div className="mapouter">
 							<div className="gmap_canvas">
-								<iframe title = "map" width="600" height="300" id="gmap_canvas" 
-								src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d7000.101514834542!2d77.1656775254471!3d28.68812822958336!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x390d03cb3bdd2fff%3A0x396a094b8bb9d820!2sORN%20Remedies%20Pvt.%20Ltd.!5e0!3m2!1sen!2sin!4v1597299689157!5m2!1sen!2sin" frameBorder="0" scrolling="no" marginHeight="0" marginWidth="0"></iframe>
+								<iframe title = "map" width="600" height="300" id="gmap_canvas" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d7000.101514834542!2d77.1656775254471!3d28.68812822958336!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x390d03cb3bdd2fff%3A0x396a094b8bb9d820!2sORN%20Remedies%20Pvt.%20Ltd.!5e0!3m2!1sen!2sin!4v1597299689157!5m2!1sen!2sin" frameBorder="0" scrolling="no" marginHeight="0" marginWidth="0"></iframe>
 							</div>
 						</div>
 					</div>
