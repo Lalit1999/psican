b2e7a1d33177b3e94e27b5c3d6290ab6e51f27a9
Manan Aggarwal <aggarwalmanan4@gmail.com>
2021-02-24 22:21:54 +0530

    t&c

--- src/comps/footer/refundPolicy/RefundPolicy.js
+++ src/comps/footer/refundPolicy/RefundPolicy.js
@@ -1,10 +1,21 @@
 import React, { Component } from 'react';
 
+import Title from '../../title/Title.js' ;
+import DisplayDetailed from '../../display/DisplayDetailed.js' ;
+
+const refund = [
+'All payments are only done before accessing the psychometric tests, only on successful completion of payment, will the aforementioned tests be opened.',
+'Once a Payment is made and test has started, no provision of cancellation is provided.',
+'No Refund/Cancellation of the payment is allowed, once user has started the test/made the payment',
+'htmlFor any kind of grievance or queries related to Payments / Refund / Cancellation, write to us at: <b>info.psyment@gmail.com</b>'
+] ;
+
 class RefundPolicy extends Component {
 	render() {
 		return (
 			<div>
-				This is RefundPolicy
+				<Title name = 'Refund/Cancellation Policy' items={["Home -","Refund Policy"]}/>
+				<DisplayDetailed title="PSYMENT Refund/Cancellation Policy" lidata={refund} />
 			</div>
 		);
 	}
--- src/comps/footer/termsCondition/TermsCondition.js
+++ src/comps/footer/termsCondition/TermsCondition.js
@@ -1,10 +1,23 @@
 import React, { Component } from 'react';
 
+import Title from '../../title/Title.js' ;
+import DisplayDetailed from '../../display/DisplayDetailed.js' ;
+
+const tc = [
+'htmlThis site and the information, names, images, pictures, logos regarding or relating to PSYMENT are provided &quot;as is&quot; without any representation or endorsement made and without warranty of any kind whether express or implied.',
+'You agree to use this site only for lawful purposes, and in a manner which does not infringe the rights, or restrict, or inhibit the use and enjoyment of the site by any third party.',
+'You agree not to reproduce, duplicate, copy, sell, resell or exploit any portion, service or access of the website through which these services are provided, without explicit written permission by us',
+'You may not use our products for any illegal or unauthorized purpose nor may you, in the use of the Service, violate any laws in your jurisdiction (including but not limited to copyright laws).',
+'A breach or violation of any of the Terms will may result in suitable legal action, in accordance with the violation.',
+'PSYMENT reserves the rights to change these terms and conditions at any time by posting changes online. Your continued use of this site after changes are posted constitutes your acceptance of the modifications.',
+] ;
+
 class TermsCondition extends Component {
 	render() {
 		return (
 			<div>
-				This is TermsCondition
+				<Title name = 'Terms and Conditions' items={["Home -","Terms & Conditions"]}/>
+				<DisplayDetailed title="PSYMENT Website Terms & Conditions" lidata={tc} />
 			</div>
 		);
 	}
