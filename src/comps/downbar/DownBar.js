eb89495a5fa6cc96edf41b78f651f06a0326ec0e
Lalit1999 <9lalitkumar9@gmail.com>
2022-12-30 11:01:32 +0530

    added images in downbar

--- src/comps/downbar/DownBar.js
+++ src/comps/downbar/DownBar.js
@@ -4,7 +4,7 @@ const DownBar = () => {
 	return (
 		<div className="downbar"> 
 			<div> Copyright &copy; 2022 &ensp;|&ensp; Website Created by &emsp;
-				<a href="https://myarth.in"><img className="myarth" src={'https://raw.githubusercontent.com/manan999/manan999.github.io/master/logo-black.png'} alt="myarth" /></a>
+				<a href="https://myarth.in"><img className="myarth" src={'https://api.myarth.in/static/images/Logo/logo-black.png'} alt="myarth" /></a>
 			</div>
 		</div>
 	);
